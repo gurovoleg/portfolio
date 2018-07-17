db0fa15ab92134fecd7d7860da6afc589adc59d5
gurovoleg <gogotheog@gmail.com>
2018-07-17 18:57:08 +0300

    секция header

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
-
+	// slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a, a.mouse_scroll").mPageScroll2id({
+	     //offset:50 //Задаем отступ сверху например для мобильного меню
+	    // clickedClass: "mPS2id-target-last"	// Устанавливает класс для ссылки, при клике на неё
+		// targetClass: "active-item"	// Устанавливает класс для блока, при его появлении во время скролла
+	});
+	
 	
 });
+
---
+++ docs/js/mobile-menu.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	var $mobileIcon = $('#mobile-menu__icon');
+	var $navList = $('.navigation__list');
+	var $navLink = $('.navigation__list a');
+	var navListOpened = 'mobile-menu--opened';
+	var IconAnimation = 'is-opened';
+
+	// открываем мобильное меню
+	$($mobileIcon).on('click', function(){
+		$(this).toggleClass(IconAnimation);
+		$($navList).toggleClass(navListOpened);
+	});
+
+	// убираем мобильное меню по клику на ссылки
+	$($navLink).on('click', function(){
+		$($mobileIcon).removeClass(IconAnimation);
+		$($navList).removeClass(navListOpened);
+	});
+
+		$(window).resize(function(){
+		var $w = $(window).width();
+		if ($w > 768) {
+			$($navList).removeClass(navListOpened);	
+			$($mobileIcon).removeClass(IconAnimation);	
+		}
+	});	
+
+});	
