551af6b8537223dc42182642a6b2dc945e5e30e9
gurovoleg <gogotheog@gmail.com>
2018-07-25 17:48:00 +0300

    исправление ошибок и доработки

--- docs/js/main.js
+++ docs/js/main.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
 	  {
 	    boxClass:     'wow',      // animated element css class (default is wow)
 	    animateClass: 'animated', // animation css class (default is animated)
-	    offset:       100,          // distance to the element when triggering the animation (default is 0)
+	    offset:       80,          // distance to the element when triggering the animation (default is 0)
 	    mobile:       true,       // trigger animations on mobile devices (default is true)
 	    live:         true,       // act on asynchronously loaded content (default is true)
 	    callback:     function(box) {
